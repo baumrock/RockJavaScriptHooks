c9340f76ac20efeb67aaf26001aa54ad09ac051a
BernhardBaumrock <office@baumrock.com>
2024-10-06 16:57:13 +0200

    refactor to use non-proxy version to support AlpineJS components

--- Hooks.js
+++ Hooks.js
@@ -8,6 +8,8 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
     before: {},
   };
 
+  class ProcessWireHooks {}
+
   // HookEvent class to use in hooks
   // eg event.arguments() or event.return
   class HookEvent {
@@ -52,117 +54,24 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
     ProcessWire.hooks.before[name] = hooks;
   };
 
-  // HookHandler is a Proxy that intercepts every method call
-  // and delegates it to the corresponding hookable method, if it
-  // exists. For example calling .foo() will delegate to ___foo()
-  const HookHandler = {
-    get: function (data, prop) {
-      const object = data.object;
-      if (typeof prop !== "string") return object[prop];
-
-      // build hook selector
-      let hookObjectName = data.name;
-      if (!hookObjectName) hookObjectName = object.constructor.name;
-      const selector = `${hookObjectName}::${prop}`;
-      // console.log(selector);
-
-      // if prop starts with ___ we return the original value
-      if (prop.startsWith("___")) return object[prop];
-
-      // if ___prop is not defined we return the original value
-      if (typeof object[`___${prop}`] === "undefined") return object[prop];
-
-      // if prop does not start with ___ we return a function that executes
-      // hooks and the original method
-      return function (...args) {
-        let replace = false;
-        let result = null;
-        let hookArgs = args;
-
-        // execute before hooks
-        const beforeHooks = ProcessWire.hooks.before[selector] || [];
-        beforeHooks.forEach((hook) => {
-          if (replace) return;
-          const evt = new HookEvent(hookArgs, result);
-          hook.fn(evt);
-          hookArgs = evt.args;
-          if (evt.replace) replace = true;
-        });
-
-        result = object[`___${prop}`].apply(this, hookArgs);
-        if (replace) return result;
-
-        // execute after hooks
-        const afterHooks = ProcessWire.hooks.after[selector] || [];
-        afterHooks.forEach((hook) => {
-          const evt = new HookEvent(hookArgs, result);
-          hook.fn(evt);
-          hookArgs = evt.args;
-          result = evt.return;
-        });
-
-        return result;
-      };
-    },
-  };
-
-  ProcessWire.wireNoProxy = function (comp, name) {
-    // get all defined of provided alpine component
-    let props = Object.getOwnPropertyDescriptors(comp);
-
-    // loop over all props and check for hookable methods
-    for (let key in props) {
-      if (!key.startsWith("___")) continue;
-
-      const val = props[key].value;
-      if (!typeof val === "function") continue;
-
-      // add new method without the ___ prefix
-      let newKey = key.slice(3);
-
-      // define the new method dynamically
-      props[newKey] = {
-        get() {
-          return function (...args) {
-            // build hook name
-            const hookName = `${name}::${newKey}`;
-
-            // build hookEvent
-            const hookEvent = {
-              object: this,
-              replace: false,
-              return: val,
-              arguments: args,
-            };
-
-            // add hook to ProcessWire to make it hookable
-            // this method call will return the initial value
-            // of the non-hooked method, but before it does that
-            // all beforeHooks are executed and after it does that
-            // all afterHooks are executed.
-            return ProcessWire.hook(hookName, hookEvent);
-          };
-        },
-      };
+  // Add this helper function to execute hooks
+  ProcessWire.executeHooks = function (type, hookName, hookEvent) {
+    const hooks = ProcessWire.hooks[type][hookName] || [];
+    for (let hook of hooks) {
+      try {
+        hook.fn(hookEvent);
+        if (hookEvent.replace && type === "before") break;
+      } catch (error) {
+        console.error(`Error in ${type} hook for ${hookName}:`, error);
+        console.log("Hook:", hook);
+        console.log("HookEvent:", hookEvent);
+      }
     }
-
-    // create the new object and return it
-    // this is using the following alpinejs workaround for nextTick issue:
-    // https://github.com/alpinejs/alpine/discussions/1940#discussioncomment-6586765
-    return Object.create(Object.getPrototypeOf(comp), props);
-  };
-
-  ProcessWire.hook = function (name, hookEvent) {
-    console.log("--- attach hook " + name + " ---");
-    console.log("hookEvent", hookEvent);
-    console.log("rctype", hookEvent.object.$rctype);
-    console.log("--- end hook ---");
-    return hookEvent.return;
   };
 
   // wire() method to apply HookHandler to an object
   // this is all we need to make any object hookable :)
-  ProcessWire.wire = function (object, name = null, noProxy = false) {
+  ProcessWire.wire = function (object, name = null) {
     // if no name is provided check if we can get it from the object
     if (!name) name = object.constructor.name;
 
@@ -176,19 +85,38 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
       );
     }
 
-    // If noProxy is set we use a different technique to add hooks to our
-    // object. This is necessary for alpinejs, because the proxy would mess up
-    // with alpine's internal proxy mechanism.
-    if (noProxy) return this.wireNoProxy(object, name);
+    let props = Object.getOwnPropertyDescriptors(object);
+    for (let key in props) {
+      if (!key.startsWith("___")) continue;
 
-    // otherwise we create a proxy object that uses the HookHandler
-    // to intercept method calls and execute hooks
-    return new Proxy(
-      {
-        object: object,
-        name: name,
-      },
-      HookHandler
-    );
+      const originalMethod = props[key].value;
+      if (typeof originalMethod !== "function") continue;
+
+      let newKey = key.slice(3);
+      const hookName = `${name}::${newKey}`;
+
+      props[newKey] = {
+        value: function (...args) {
+          // Create hookEvent object using HookEvent class
+          const hookEvent = new HookEvent(args, undefined, this);
+
+          // Execute before hooks
+          ProcessWire.executeHooks("before", hookName, hookEvent);
+
+          // if event.replace is true we do not call the original method
+          if (hookEvent.replace) return hookEvent.return;
+
+          // Call the original method
+          hookEvent.return = originalMethod.apply(this, hookEvent.arguments());
+
+          // Execute after hooks
+          ProcessWire.executeHooks("after", hookName, hookEvent);
+
+          return hookEvent.return;
+        },
+      };
+    }
+
+    return Object.create(Object.getPrototypeOf(object), props);
   };
 })();
