c70459fe969ff108bd521798c3eebb958dab8785
BernhardBaumrock <office@baumrock.com>
2024-10-06 17:37:06 +0200

    refactor to use ProcessWireHooks class

--- Hooks.js
+++ Hooks.js
@@ -9,6 +9,20 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
       before: {},
     };
 
+    static addHookAfter(name, fn, priority = 100) {
+      let hooks = ProcessWireHooks.hooks.after[name] || [];
+      hooks.push(new Hook(name, fn, priority));
+      hooks.sort((a, b) => a.priority - b.priority);
+      ProcessWireHooks.hooks.after[name] = hooks;
+    }
+
+    static addHookBefore(name, fn, priority = 100) {
+      let hooks = ProcessWireHooks.hooks.before[name] || [];
+      hooks.push(new Hook(name, fn, priority));
+      hooks.sort((a, b) => a.priority - b.priority);
+      ProcessWireHooks.hooks.before[name] = hooks;
+    }
+
     static executeHooks(type, hookName, hookEvent) {
       const hooks = ProcessWireHooks.hooks[type][hookName] || [];
       for (let hook of hooks) {
@@ -22,16 +36,73 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
         }
       }
     }
+
+    // wire() method to apply HookHandler to an object
+    // this is all we need to make any object hookable :)
+    static wire = function (object, name = null) {
+      // if no name is provided check if we can get it from the object
+      if (!name) name = object.constructor.name;
+
+      // if the object is not a class it will have name "Object"
+      // in that case we throw an error so that the developer provides a name
+      // that we can use for the hook identifier like "Foo::hello" or otherwise
+      // all generic objects would have the same hook name "Object::hello"
+      if (name === "Object") {
+        throw new Error(
+          "Please provide a name in ProcessWire.wire(object, name)"
+        );
+      }
+
+      let props = Object.getOwnPropertyDescriptors(object);
+      for (let key in props) {
+        if (!key.startsWith("___")) continue;
+
+        const originalMethod = props[key].value;
+        if (typeof originalMethod !== "function") continue;
+
+        let newKey = key.slice(3);
+        const hookName = `${name}::${newKey}`;
+
+        props[newKey] = {
+          value: function (...args) {
+            // Create hookEvent object using HookEvent class
+            const hookEvent = new HookEvent({
+              arguments: args,
+              object: this,
+            });
+
+            // Execute before hooks
+            ProcessWireHooks.executeHooks("before", hookName, hookEvent);
+
+            // if event.replace is true we do not call the original method
+            if (hookEvent.replace) return hookEvent.return;
+
+            // Call the original method
+            hookEvent.return = originalMethod.apply(
+              this,
+              hookEvent.arguments()
+            );
+
+            // Execute after hooks
+            ProcessWireHooks.executeHooks("after", hookName, hookEvent);
+
+            return hookEvent.return;
+          },
+        };
+      }
+
+      return Object.create(Object.getPrototypeOf(object), props);
+    };
   }
 
   // HookEvent class to use in hooks
   // eg event.arguments() or event.return
   class HookEvent {
-    constructor(args, data, object) {
-      this.object = object;
-      this._arguments = args;
+    constructor(data) {
+      this.object = data.object;
+      this._arguments = data.arguments;
       this.replace = false;
-      this.return = data;
+      this.return = data.return;
     }
 
     // dynamic arguments getter
@@ -82,70 +153,8 @@ if (typeof ProcessWire == "undefined") ProcessWire = {};
     }
   }
 
-  // Function to add an after hook for a given event name
-  ProcessWire.addHookAfter = function (name, fn, priority = 100) {
-    let hooks = ProcessWireHooks.hooks.after[name] || [];
-    hooks.push(new Hook(name, fn, priority));
-    hooks.sort((a, b) => a.priority - b.priority);
-    ProcessWireHooks.hooks.after[name] = hooks;
-  };
-
-  // Function to add a before hook for a given event name
-  ProcessWire.addHookBefore = function (name, fn, priority = 100) {
-    let hooks = ProcessWireHooks.hooks.before[name] || [];
-    hooks.push(new Hook(name, fn, priority, "before"));
-    hooks.sort((a, b) => a.priority - b.priority);
-    ProcessWireHooks.hooks.before[name] = hooks;
-  };
-
-  // wire() method to apply HookHandler to an object
-  // this is all we need to make any object hookable :)
-  ProcessWire.wire = function (object, name = null) {
-    // if no name is provided check if we can get it from the object
-    if (!name) name = object.constructor.name;
-
-    // if the object is not a class it will have name "Object"
-    // in that case we throw an error so that the developer provides a name
-    // that we can use for the hook identifier like "Foo::hello" or otherwise
-    // all generic objects would have the same hook name "Object::hello"
-    if (name === "Object") {
-      throw new Error(
-        "Please provide a name in ProcessWire.wire(object, name)"
-      );
-    }
-
-    let props = Object.getOwnPropertyDescriptors(object);
-    for (let key in props) {
-      if (!key.startsWith("___")) continue;
-
-      const originalMethod = props[key].value;
-      if (typeof originalMethod !== "function") continue;
-
-      let newKey = key.slice(3);
-      const hookName = `${name}::${newKey}`;
-
-      props[newKey] = {
-        value: function (...args) {
-          // Create hookEvent object using HookEvent class
-          const hookEvent = new HookEvent(args, undefined, this);
-
-          // Execute before hooks
-          ProcessWireHooks.executeHooks("before", hookName, hookEvent);
-
-          // if event.replace is true we do not call the original method
-          if (hookEvent.replace) return hookEvent.return;
-
-          // Call the original method
-          hookEvent.return = originalMethod.apply(this, hookEvent.arguments());
-
-          // Execute after hooks
-          ProcessWireHooks.executeHooks("after", hookName, hookEvent);
-
-          return hookEvent.return;
-        },
-      };
-    }
-
-    return Object.create(Object.getPrototypeOf(object), props);
-  };
+  // ##### Public ProcessWire API #####
+  ProcessWire.addHookAfter = ProcessWireHooks.addHookAfter;
+  ProcessWire.addHookBefore = ProcessWireHooks.addHookBefore;
+  ProcessWire.wire = ProcessWireHooks.wire;
 })();
