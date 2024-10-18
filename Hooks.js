01dcc9b67dce3e5211f35b3fc31cce5f24637971
BernhardBaumrock <office@baumrock.com>
2024-10-18 21:16:17 +0200

    remove old version and comments

--- Hooks.js
+++ Hooks.js
@@ -1,71 +1,12 @@
+// create global ProcessWire object if it doesn't exist
+if (typeof ProcessWire == "undefined") ProcessWire = {};
+
 (() => {
   // Initialize hooks object to store before and after hooks
   ProcessWire.hooks = {
     after: {},
     before: {},
   };
-
-  // Hook class to create hook objects with name, function, and priority
-  class Hook {
-    constructor(name, fn, priority = 100) {
-      this.name = name;
-      this.fn = fn;
-      this.priority = priority;
-    }
-  }
-
-  // HookEvent class to create event objects with name and data
-  class HookEventOld {
-    constructor(name, data, context) {
-      this.name = name;
-      this.replace = false;
-      this.return = data;
-      this.context = context;
-    }
-  }
-
-  // Function to add an after hook for a given event name
-  ProcessWire.addHookAfter = function (name, fn, priority = 100) {
-    let hooks = ProcessWire.hooks.after[name] || [];
-    hooks.push(new Hook(name, fn, priority));
-    hooks.sort((a, b) => a.priority - b.priority);
-    ProcessWire.hooks.after[name] = hooks;
-  };
-
-  // Function to add a before hook for a given event name
-  ProcessWire.addHookBefore = function (name, fn, priority = 100) {
-    let hooks = ProcessWire.hooks.before[name] || [];
-    hooks.push(new Hook(name, fn, priority, "before"));
-    hooks.sort((a, b) => a.priority - b.priority);
-    ProcessWire.hooks.before[name] = hooks;
-  };
-
-  // Function to execute hooks for a given event name and data
-  ProcessWire.hookable = function (name, data, context) {
-    let hooksBefore = ProcessWire.hooks.before[name] || [];
-    let hooksAfter = ProcessWire.hooks.after[name] || [];
-    let event = new HookEventOld(name, data, context);
-
-    // Execute before hooks
-    hooksBefore.forEach((hook) => {
-      // if previous hook has replace = true skip this one
-      if (event.replace) return;
-      hook.fn(event, context);
-    });
-
-    // If event has replace = true, return the new data
-    if (event.replace) return event.return;
-
-    // Execute after hooks
-    hooksAfter.forEach((hook) => {
-      hook.fn(event, context);
-    });
-
-    // Return the final event data
-    return event.return;
-  };
-
-  /** ##### new version ##### */
 
   class HookEvent {
     constructor(args, data) {
@@ -131,78 +72,3 @@
     return new Proxy(object, HookHandler);
   };
 })();
-
-// class HelloWorld {
-//   ___greet(salut = "hello", what = "world") {
-//     return `${salut} ${what}`;
-//   }
-// }
-
-// const helloWorld = ProcessWire.wire(new HelloWorld());
-
-// // shows hello world
-// console.log(helloWorld.greet());
-
-// // shows hi there
-// console.log(helloWorld.greet("hi", "there"));
-
-// // add BEFORE hook
-// ProcessWire.addHookBefore("HelloWorld::greet", (event) => {
-//   event.arguments(0, "hallo");
-//   event.arguments(1, "welt");
-// });
-
-// // shows hallo welt
-// console.log(helloWorld.greet());
-
-// // shows hallo welt
-// console.log(helloWorld.greet("servas", "oida"));
-
-// // add AFTER hook
-// ProcessWire.addHookAfter("HelloWorld::greet", (event) => {
-//   // shows ['hallo', 'welt']
-//   console.log(event.arguments());
-//   event.return = "hi universe";
-// });
-
-// // shows hi universe
-// console.log(helloWorld.greet());
-
-// console.log("----------- hook priority -----------");
-
-// class PrioDemo {
-//   ___greet() {
-//     return "hello world";
-//   }
-// }
-
-// const prio = ProcessWire.wire(new PrioDemo());
-
-// ProcessWire.addHookAfter(
-//   "PrioDemo::greet",
-//   () => {
-//     console.log("second");
-//   },
-//   20
-// );
-// ProcessWire.addHookAfter(
-//   "PrioDemo::greet",
-//   () => {
-//     console.log("first");
-//   },
-//   10
-// );
-// ProcessWire.addHookAfter(
-//   "PrioDemo::greet",
-//   () => {
-//     console.log("third");
-//   },
-//   30
-// );
-
-// // shows
-// // first
-// // second
-// // third
-// // hello world
-// console.log(prio.greet());
